19c7c7a35e0d7224118c3ba275af571d12e64b40
Gina Sekhar <ginasekhar@yahoo.com>
2019-10-11 13:20:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,8 +21,7 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese (snacks)
-  # code an argument here
-  # the array below is here to help
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
   
   snacks.each do |snack|
